c699d33146714c83ebc17ae61645bb2cc20f5472
DT227 <denny900227@gmail.com>
2023-03-05 16:16:15 +0800

    專案初始化

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-
 
 client.login(process.env.TOKEN);
